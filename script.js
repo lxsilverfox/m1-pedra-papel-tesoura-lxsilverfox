070b570ff653f0c56d36a028a198a18df7ac203e
lxsilverfox <lucasxxavier315@gmail.com>
2024-05-19 14:36:57 -0300

    function playRockPaperScissor criada

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-/* Desenvolva seu código abaixo */
+function playRockPaperScissor(player1, player2) {
+    const opcao1 = "Pedra";
+    const opcao2 = "Papel";
+    const opcao3 = "Tesoura";
+
+    if (player1 == player2) {
+        return "Empate!";
+    } else if (
+        (player1 == opcao1 && player2 == opcao3) ||
+        (player1 == opcao2 && player2 == opcao1) ||
+        (player1 == opcao3 && player2 == opcao2)
+    ) {
+        return "Jogador 1 venceu!";
+    } else {
+        return "Jogador 2 venceu!";
+    }
+}
+
+console.log(playRockPaperScissor("Pedra", "Papel"));
